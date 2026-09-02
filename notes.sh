6054650fbda401327e10bf5c9459757a25c48473
Jorge Luis Perez Flores <jorge.flores@accionlabs.com>
2026-09-02 12:24:26 -0500

    Changes

---
+++ notes.sh
@@ -0,0 +1,6 @@
+$env:Path += ";C:\Users\jorge.flores\Documents\python"
+
+git add .
+git commit -m "Changes"
+#git remote add origin https://github.com/USUARIO/REPOSITORIO.git
+git push -u origin master
